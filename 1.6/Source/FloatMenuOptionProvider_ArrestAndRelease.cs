e5a59bccaf54b83470d8b513cc6562df0eb61d44
1trickPwnyta <1trickPwnyta@kangaroostandard.com>
2025-06-14 13:35:14 -0700

    Update to 1.6

---
+++ 1.6/Source/FloatMenuOptionProvider_ArrestAndRelease.cs
@@ -0,0 +1,61 @@
+﻿using RimWorld;
+using System.Linq;
+using Verse;
+using Verse.AI;
+
+namespace ArrestAndRelease
+{
+    public class FloatMenuOptionProvider_ArrestAndRelease : FloatMenuOptionProvider_Arrest
+    {
+        protected override FloatMenuOption GetSingleOptionFor(Pawn clickedPawn, FloatMenuContext context)
+        {
+            Pawn arrestor = context.FirstSelectedPawn;
+
+            if (!clickedPawn.CanBeArrestedBy(arrestor))
+            {
+                return null;
+            }
+
+            if (clickedPawn.IsPrisoner)
+            {
+                return null;
+            }
+
+            if (clickedPawn.Downed && clickedPawn.guilt.IsGuilty)
+            {
+                return null;
+            }
+
+            if (ArrestAndReleaseUtil.IsUnderArrestAndRelease(clickedPawn))
+            {
+                return null;
+            }
+
+            if (arrestor.InSameExtraFaction(clickedPawn, ExtraFactionType.HomeFaction) || arrestor.InSameExtraFaction(clickedPawn, ExtraFactionType.MiniFaction))
+            {
+                return new FloatMenuOption("ArrestAndRelease_CannotArrestAndRelease".Translate() + ": " + "SameFaction".Translate(clickedPawn), null);
+            }
+
+            if (!arrestor.CanReach(clickedPawn, PathEndMode.OnCell, Danger.Deadly))
+            {
+                return new FloatMenuOption("ArrestAndRelease_CannotArrestAndRelease".Translate() + ": " + "NoPath".Translate().CapitalizeFirst(), null);
+            }
+
+            return FloatMenuUtility.DecoratePrioritizedTask(new FloatMenuOption("ArrestAndRelease_TryToArrestAndRelease".Translate(clickedPawn.LabelShortCap, clickedPawn.GetAcceptArrestChance(arrestor).ToStringPercent()), () =>
+            {
+                bool possiblePrison = clickedPawn.Map.regionGrid.AllRooms.Any(r => r.IsSuitableForArrest(arrestor, clickedPawn));
+                if (possiblePrison)
+                {
+                    RCellFinder.TryFindPrisonerReleaseCell(clickedPawn, arrestor, out IntVec3 releaseCell);
+                    Job job = JobMaker.MakeJob(JobDefOf.ArrestAndRelease, clickedPawn, releaseCell);
+                    job.count = 1;
+                    arrestor.jobs.TryTakeOrderedJob(job);
+                }
+                else
+                {
+                    Messages.Message("CannotArrest".Translate() + ": " + "ArrestAndRelease_NoPotentialPrison".Translate(), clickedPawn, MessageTypeDefOf.RejectInput, false);
+                }
+            }), arrestor, clickedPawn);
+        }
+    }
+}
---
+++ 1.6/Source/Refs.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+
+namespace ArrestAndRelease
+{
+    public static class Refs
+    {
+        public static MethodInfo m_List_FloatMenuOption_Add = AccessTools.Method(typeof(List<FloatMenuOption>), "Add");
+        public static MethodInfo m_Pawn_get_IsSlave = AccessTools.Method(typeof(Pawn), "get_IsSlave");
+        public static MethodInfo m_ArrestAndReleaseUtil_IsUnderArrestAndRelease = AccessTools.Method(typeof(ArrestAndReleaseUtil), "IsUnderArrestAndRelease");
+        public static MethodInfo m_Pawn_get_IsPrisoner = AccessTools.Method(typeof(Pawn), "get_IsPrisoner");
+    }
+}
